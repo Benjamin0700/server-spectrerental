851c335fd2035acb3eed64a9b7e5204ccb88a72c
Jaloliddin <jalolsoxiddinov777@gmail.com>
2025-05-24 16:55:11 +0500

    Update Camera.js

--- models/Camera.js
+++ models/Camera.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const cameraSchema = new mongoose.Schema({
   name: { type: String, required: true },
   title: { type: String, required: true },
-  imageUrl: { type: String, required: true },
+  upload: { type: String, required: true },
 }, { timestamps: true });
 
 const Camera = mongoose.model('Image', cameraSchema);
